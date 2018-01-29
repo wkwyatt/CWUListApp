4d212ce9cb9b0db1db40f19f77e9df24bd271e36
William Wyatt <wkwyatt@Williams-MacBook-Pro.local>
2018-01-29 11:11:59 -0500

    - add items to the TableView dynamically - delete items from the TableView dynamically

--- CWUListApp/TableViewController.swift
+++ CWUListApp/TableViewController.swift
@@ -30,8 +30,39 @@ class TableViewController: UITableViewController {
     }
     
     @IBAction func addButtonTouched(_ sender: Any) {
-        print("test")
+        // Add an alert box
+        let alert  = UIAlertController(title: "Name your new Emoji Sticker", message: nil, preferredStyle: .alert)
+        
+        // Create a text input field where a user can input text
+        alert.addTextField { (emojiNameTF) in emojiNameTF.placeholder = "My Emoji" }
+        
+        // Add the user's input to the list we have
+        
+        // 1. Create an "add" alert button action
+        let addAction = UIAlertAction(title: "Add", style: .default) { (_) in
+            guard let emojiName = alert.textFields?.first?.text else { return }
+            // Add the emoji name to the list
+            self.addItem(emojiName)
+            print(emojiName)
+        }
+        
+        // 2. Apply the button to the alert box
+        alert.addAction(addAction)
+        
+        // Present the alert box to the user
+        present(alert, animated: true)
     }
+    
+    func addItem(_ newEmoji: String) {
+        // Insert emoji name to the top of the myEmojiStickers array
+        let index = 0
+        myEmojiNames.insert(newEmoji, at: index)
+        
+        // Insert the new rows in the tableView on the top
+        let indexPath = IndexPath(row: index, section: 0)
+        self.tableView.insertRows(at: [indexPath], with: .left)
+    }
+
     
     // MARK: - Table view data source
 
@@ -64,17 +95,18 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            myEmojiNames.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
